02c03bca71c247c484e51cbf294e77778eeac371
Dante <dante383@protonmail.com>
2016-08-03 18:53:31 +0200

    initial commit

---
+++ class_exif.php
@@ -0,0 +1,111 @@
+<?php 
+
+class Exif 
+{
+	
+	public function getImageType ($i) // $i - image path
+	{
+		if (!isset($i))
+		{
+			return false;
+		}
+		if (is_string($i) == false)
+		{
+			return false;
+		}
+		$type = exif_imagetype($i);
+		if ($type == false)
+		{
+			return false;
+		}
+		 return $type;
+	}
+	
+	public function getExifValues ($i, $sections = NULL, $arrays = false, $thumbnail = false)
+	{
+		if (!isset($i))
+		{
+			return false;
+		}
+		if (is_string($i) == false)
+		{
+			return false;
+		}
+		if ($sections != NULL)
+		{
+			if (is_string($sections) == false)
+			{
+				return false;
+			}
+		}
+		if ($arrays != false)
+		{
+			if (is_array($arrays) == false)
+			{
+				return false;
+			}
+		}
+		$data = exif_read_data($i, $sections, $arrays, $thumbnail);
+		if ($data == false)
+		{
+			return false;
+		}
+		return $data;
+	}
+	
+	public function getExifValue ($i, $key)
+	{
+		if (!isset($i) || !isset($key))
+		{
+			return false;
+		}
+		if (is_string($i) == false || is_string($key) == false)
+		{
+			return false;
+		}
+		$values = exif_read_data($i);
+		if ($values == false)
+		{
+			return false;
+		}
+		if (!isset($values[$key]))
+		{
+			return false;
+		}
+		return $values[$key];
+	}
+	
+	public function getExifTagName ($index)
+	{
+		if (is_numeric($index) == false)
+		{
+			return false;
+		}
+		$tagName = exif_tagname($index);
+		if ($tagName == false)
+		{
+			return false;
+		}
+		return $tagName;
+	}
+	
+	public function getExifThumbnail ($i, $width = null, $height = null, $imagetype = null)
+	{
+		if (!isset($i))
+		{
+			return false;
+		}
+		if (is_string($i) == false)
+		{
+			return false;
+		}
+		$thumbnail = exif_thumbnail($i, $width, $height, $imagetype);
+		if ($thumbnail == false)
+		{
+			return false;
+		}
+		return $thumbnail;
+	}
+	
+	
+}
---
+++ index.php
@@ -0,0 +1,53 @@
+<?php 
+require_once('class_exif.php');
+
+$e = new Exif();
+
+echo '<img src="elliot.jpg">';
+
+echo '<h2>Image Type</h2>';
+$imageType = $e->getImageType('elliot.jpg');
+if ($imageType != false)
+{
+	echo $imageType;
+}
+echo '<h2>All exif values</h2>';
+$values = $e->getExifValues('elliot.jpg');
+if($values != false)
+{
+	foreach($values as $key => $value)
+	{
+		if (is_array($value) == false)
+		{
+			echo $key.': '.$value.'<br>';
+		}
+		else
+		{
+			echo $key.': '; 
+			var_dump($value);
+			echo '<br>';
+		}
+	}
+}
+
+echo '<h2>Single exif value, for example MimeType</h2>';
+$value = $e->getExifValue('elliot.jpg', 'MimeType');
+if ($value != false)
+{
+	echo $value;
+}
+echo '<h2>Tag name, for example 256</h2>';
+$tagName = $e->getExifTagName(256);
+if ($tagName != false)
+{
+	echo $tagName;
+}
+
+echo '<h2>Image thumbnail below but jpg does not support it (?). Change image to see effects.</h2>';
+$thumbnail = $e->getExifThumbnail('elliot.jpg');
+if ($thumbnail != false)
+{
+	echo $thumbnail;
+}
+
+?>
